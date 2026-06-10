4c418d34cae74ee3f8f309f0d1c9e31012d8face
ObaidAbdullah16 <obaidabdullah1610@gmail.com>
2026-06-11 00:53:58 +0530

    add start/stop scripts and fix ECR force_delete

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -45,6 +45,7 @@ locals {
 resource "aws_ecr_repository" "app" {
   name                 = var.project_name
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
